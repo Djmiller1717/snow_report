cb2b76ba23b9c6c1832e778e35a1db07b0ec11f7
Doug Miller <Djmiller1717@gmail.com>
2020-11-10 23:29:08 -0500

    hard-coded resorts removed from map component

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -1,72 +1,7 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
 import { connect } from 'react-redux';
-// import { Marker } from 'react-mapbox-gl';
 import { fetchedResorts } from '../redux/resorts';
-// import resorts from '../../seed';
-
-const resorts = [
-  {
-    resortName: 'Hunter Mountain',
-    location: [-74.2246, 42.2029],
-    state: 'New York',
-  },
-  {
-    resortName: 'Wildcat Mountain',
-    location: [-71.2393, 44.2641],
-    state: 'New Hampshire',
-  },
-  {
-    resortName: 'Mount Sunapee',
-    location: [-72.0742, 43.3136],
-    state: 'New Hampshire',
-  },
-  {
-    resortName: 'Jack Frost / Big Boulder',
-    location: [-75.6563, 41.1092],
-    state: 'Pennsylvania',
-  },
-  {
-    resortName: 'Whitetail Resort',
-    location: [-77.9333, 39.7418],
-    state: 'Pennsylvania',
-  },
-  {
-    resortName: 'Roundtop Mountain Resort',
-    location: [-76.9275, 40.1095],
-    state: 'Pennsylvania',
-  },
-  {
-    resortName: 'Attitash Mountain Resort',
-    location: [-71.2294, 44.0828],
-    state: 'New Hampshire',
-  },
-  {
-    resortName: 'Crotched Mountain',
-    location: [-71.8737, 42.9984],
-    state: 'New Hampshire',
-  },
-  {
-    resortName: 'Liberty Mountain Resort',
-    location: [-77.3755, 39.7637],
-    state: 'Pennsylvania',
-  },
-  {
-    resortName: 'Okemo',
-    location: [-72.7170, 43.4018],
-    state: 'Vermont',
-  },
-  {
-    resortName: 'Mount Snow',
-    location: [-72.9204, 42.9602],
-    state: 'Vermont',
-  },
-  {
-    resortName: 'Stowe',
-    location: [-72.6874, 44.4654],
-    state: 'Vermont',
-  },
-];
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGptaWxsZXIxNzE3IiwiYSI6ImNrZHFvMHoyMzAwZGwycW54dXN1cTFveW8ifQ.UHW9oi2Z8NPas_liUEiAnw';
 let map = {};
@@ -84,7 +19,7 @@ class Map extends Component {
 
   componentDidMount() {
     const { lng, lat, zoom } = this.state;
-    const { fetchedResorts } = this.props;
+    const { fetchedResorts, resorts } = this.props;
     map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v11',
@@ -92,22 +27,7 @@ class Map extends Component {
       zoom,
     });
 
-    // fetchedResorts();
-    // console.log(resorts);
-    // if (resorts.length) {
-    map.on('load', () => {
-      // let marker = new mapboxgl.Marker()
-      //   .setLngLat([-74, 42.5])
-      //   .addTo(map);
-      resorts.forEach((resort) => {
-        let marker = new mapboxgl.Marker()
-          .setLngLat(resort.location)
-          .addTo(map);
-      //   console.log(marker);
-      //   return marker;
-      });
-    });
-    // }
+    fetchedResorts();
 
     map.on('move', () => {
       this.setState({
@@ -120,7 +40,21 @@ class Map extends Component {
 
   render() {
     const { lng, lat, zoom } = this.state;
-    // const { resorts } = this.props;
+    const { resorts } = this.props;
+    if (resorts.length) {
+      map.on('load', () => {
+        // let marker = new mapboxgl.Marker()
+        //   .setLngLat([-74, 42.5])
+        //   .addTo(map);
+        resorts.forEach((resort) => {
+          let marker = new mapboxgl.Marker()
+            .setLngLat(resort.location)
+            .addTo(map);
+        //   console.log(marker);
+        //   return marker;
+        });
+      });
+    }
     return (
       <div>
         <div className="sidebarStyle">
